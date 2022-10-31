77d5354cb611e6c066b4e7b19f2b624d3644d86e
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2022-10-31 14:31:51 -0300

    deploy changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true
 };
+export const API_PATH = "https://swapi.dev/api"
+
+export const API_CHARACTERS_IMAGES = "https://akabab.github.io/starwars-api/api/id/"
